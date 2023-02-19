4fa719bce699d077b150171fe3d4e983456aaa5f
Bayezid075 <mrbayezidhoshen@gmail.com>
2023-02-19 15:11:12 +0600

    finish

---
+++ script.js
@@ -0,0 +1,21 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+// promt to stream media , pass to video element , then play
+async function mediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+button.addEventListener("click", async () => {
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+
+mediaStream();
